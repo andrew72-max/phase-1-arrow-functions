a9a14786198c6617e7674e3e5a47785ca4b77353
andrew72-max <andrew.nyongesa@student.moringaschool.com>
2024-11-27 14:38:45 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,9 @@
+// Define the functions
+let divide = (a, b) => a / b;
+let square = x => x * x;
+let add = (a, b) => a + b;
+
+// Use the functions
+let resultDivide = divide(2000, 100);  
+let resultSquare = square(5);          
+let resultAdd = add(10, 20);
